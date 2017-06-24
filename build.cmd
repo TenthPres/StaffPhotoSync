9f2377cf1bdc199a366e9abab7f71669283d59e3
James Kurtz <jkrrv@jkrrv.org>
2017-06-23 18:23:31 -0600

    Improved Build Procedure

--- build.cmd
+++ build.cmd
@@ -1,11 +1,17 @@
 pip install pyinstaller
 
-rmdir /s /q dist\webScrape
+rm .\StaffPhotos.zip
+
+rmdir /s /q dist
+rmdir /s /q build
+
 pyinstaller --onedir webScrape.py
 
-rmdir /s /q dist\photoFaceCrop
 pyinstaller --onedir photoFaceCrop.py
 
 copy haarcascade_frontalface_default.xml dist\haarcascade_frontalface_default.xml
 
 copy run-prod.cmd dist\run.cmd
+
+
+PowerShell -Command "Compress-Archive -Path .\dist\* -DestinationPath StaffPhotos.zip"
