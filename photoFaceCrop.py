565dfd16ab69c2c17d2e64e91c825d642c25bf89
James K <jkrrv@jkrrv.org>
2021-12-13 20:32:28 -0500

    Updates for newer standards

--- photoFaceCrop.py
+++ photoFaceCrop.py
@@ -1,5 +1,6 @@
 import cv2
 import glob
+import numpy
 
 cascPath = "haarcascade_frontalface_default.xml"
 
@@ -29,14 +30,15 @@ for file in files:
                 gray,
                 scaleFactor=scale,
                 minNeighbors=5,
-                minSize=(100, 100),
+                minSize=(50, 50),
+                maxSize=(400, 400),
                 flags=cv2.CASCADE_SCALE_IMAGE
             )
         except:
             print "!! Could not find face in {0}.".format(file)
             valid = False
 
-    print "Found {0} faces with scale {1}.".format(len(faces), scale)
+    print "Found {0} faces.".format(len(faces))
 
     # Crop Padding
     padding = .2
@@ -46,28 +48,92 @@ for file in files:
         continue
 
     # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
+    for (fx, fy, fw, fh) in faces:
         #print x, y, w, h
 
-        left = round(w * padding)
-        right = round(w * padding)
-        top = round(h * padding)
-        bottom = round(h * padding)
+        print "width: {0}.".format(fw)
 
-        # Dubugging boxes
-        # cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # half height and width
+        fw2 = fw / 2
+        fh2 = fh / 2
 
-        # Make sure intended bounds are not outside image bounds
-        # TODO rewrite this in a way that keeps things more square, but still produces a result.
-        baseH, baseW, channels = image.shape
-        y1 = max([0, y-top])
-        y2 = min([baseH, y+h+bottom])
-        x1 = max([0, x-left])
-        x2 = min([baseW, x+w+right])
+        # shifting these points so the X and Y is the center of the face
+        fx += fw2
+        fy += fh2
 
-        newImage = image[y1:y2, x1:x2]
+        # Debugging markers
+        # cv2.rectangle(image, (fx - fw2, fy - fh2), (fx + fw2, fy + fh2), (0, 255, 0), 2)
+        # image = cv2.circle(image, (fx, fy), 2, (255, 0, 0), 3)
+
+        # face size.  Used for scaling.
+        padding = max(fw, fh) * .9
+
+        baseH, baseW, n = image.shape
+        hwRatio = 17.0/12.0
+        y1 = int(fy - padding * (hwRatio / 3 * 2.5))
+        y2 = int(fy + padding * (hwRatio / 3 * 3.5))
+        x1 = int(fx - padding)
+        x2 = int(fx + padding)
+
+        print "ratio: {}".format(hwRatio)
+
+        white = numpy.full((y2-y1, x2-x1, 3), (255, 255, 255), numpy.uint8)
+        newImage = white
+        print '{} {} {} {}'.format(x1, x2, y1, y2)
+
+        if y2 > baseH:
+            print "Crop extends beyond source bounds by {} rows.".format(y2 - baseH)
+            y2 = baseH
+
+        yOff = 0
+        if y1 < 0:
+            print "Crop extends above source bounds by {} rows.".format(-y1)
+            yOff = -y1
+            y1 = 0
+
+        newImage[yOff:y2 - y1 + yOff, 0:x2 - x1] = image[y1:y2, x1:x2]
+
+        newImage = cv2.resize(newImage, (200, int(200.0 * hwRatio)))
 
         print "{0}".format(file[10:])
-        cv2.imwrite("cropped\{0}".format(file[10:]), newImage)
+        cv2.imwrite("croppedLarge\{0}".format(file[10:]), newImage)
+
+        # ################### Small Image ###################
+
+        # face size.  Used for scaling.
+        padding = max(fw, fh) * .7
+
+        baseH, baseW, n = image.shape
+        y1 = int(fy - padding)
+        y2 = int(fy + padding)
+        x1 = int(fx - padding)
+        x2 = int(fx + padding)
+
+        white = numpy.full((y2-y1, x2-x1, 3), (255, 255, 255), numpy.uint8)
+        newImage = white
+        print '{} {} {} {}'.format(x1, x2, y1, y2)
+
+        if y2 > baseH:
+            print "Crop extends beyond source bounds by {} rows.".format(y2 - baseH)
+            y2 = baseH
+
+        yOff = 0
+        if y1 < 0:
+            print "Crop extends above source bounds by {} rows.".format(-y1)
+            yOff = -y1
+            y1 = 0
+
+        newImage[yOff:y2 - y1 + yOff, 0:x2 - x1] = image[y1:y2, x1:x2]
+
+        newImage = cv2.resize(newImage, (150, 150))  # need to maintain 1:1 ratio
+
+        fileName = file[10:].split(' ', 1)
+        fileName = (fileName[0][0] + fileName[1]).lower()
+        print "{0}".format(fileName)
+        cv2.imwrite("croppedSmall\{0}".format(fileName), newImage)
+
         cnt += 1
-    print ''
+
+        break
+
+    print ''  # blank line between outputs.
